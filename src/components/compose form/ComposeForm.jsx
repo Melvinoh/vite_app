3cfd17ecaa4fd0e8c07503a633329632d2114ee2
Melvinoh <murichumelvin@gmail.com>
2023-05-26 22:48:37 +0300

    satuk web

--- src/components/compose form/ComposeForm.jsx
+++ src/components/compose form/ComposeForm.jsx
@@ -17,14 +17,19 @@ const ComposeForm = () => {
                 <label for="image"> image :</label>
                 <input type="file"  name="image"/>
             </div>
+            <div className="img-preview">
+                <img src="/pictures/babygirl.jpg" alt="" />
+            </div>
             <div className="c-in">
                 <label for="heading"> heading :</label>
                 <input type="text"  name="image"/>
             </div>
+           
             <div className="c-in">
                 <label for="image">message :</label>
                 <textarea name="message" rows="15 " columns="30"  placeholder='your text'/>
             </div>
+            <div className='btn'> <span>publish</span></div>
         </form>
     </div>
    </div>
