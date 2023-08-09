e4fa90f2a1bd9f811843ab1174147efcb8fff98a
Melvinoh <murichumelvin@gmail.com>
2023-08-09 15:02:38 +0300

    satuk app

--- src/components/forms/ProfileUpdate.jsx
+++ src/components/forms/ProfileUpdate.jsx
@@ -45,48 +45,48 @@ const ProfileUpdate = ({click}) => {
               <div className="inputs-wrap">
                 <div className="inputs-upt">
                   <label htmlFor="fname">first name</label>
-                  <input type="text" value="muturi" />
+                  <input type="text"defaultValue="muturi" />
                 </div>
                 <div className="inputs-upt">
                   <label htmlFor="sname">second name</label>
-                  <input type="text" value="melvin" />
+                  <input type="text"defaultValue="melvin" />
                 </div>
               </div>
               <div className="inputs-upt">
                 <label htmlFor="email">email</label>
-                <input type="text" value="muturimelvin@gmail.com" />
+                <input type="text" defaultValue="muturimelvin@gmail.com" />
               </div>
               <div className="inputs-upt">
                 <label htmlFor="phone">contacts number</label>
-                <input type="text" value="+254 728318369" />
+                <input type="text"defaultValue="+254 728318369" />
               </div>
               <div className="inputs-upt">
                 <label htmlFor="address">address</label>
-                <input type="text" value="930-10100 nyeri" />
+                <input type="text"defaultValue="930-10100 nyeri" />
               </div>
               <div className="inputs-wrap">
                 <div className="inputs-upt">
                   <label htmlFor="city">city</label>
-                  <input type="text" value="nairobi" />
+                  <input type="text"defaultValue="nairobi" />
                 </div>
                 <div className="inputs-upt">
                   <label htmlFor="state">state</label>
-                  <input type="text" value="madaraka" />
+                  <input type="text"defaultValue="madaraka" />
                 </div>
               </div>
               <div className="inputs-wrap">
                 <div className="inputs-upt">
                   <label htmlFor="city">next of king</label>
-                  <input type="text" value="john kelvin" />
+                  <input type="text"defaultValue="john kelvin" />
                 </div>
                 <div className="inputs-upt">
                   <label htmlFor="id">id number</label>
-                  <input type="text" value="38760186" />
+                  <input type="text"defaultValue="38760186" />
                 </div>
               </div>
               <div className="inputs-upt">
                   <label htmlFor="id">password</label>
-                  <input type="password" value="38760186" />
+                  <input type="password"defaultValue="38760186" />
               </div>
               <button>save changes</button>
             </div>
@@ -98,17 +98,17 @@ const ProfileUpdate = ({click}) => {
             <div className="inputs-wrap">
               <div className="inputs-upt">
                 <label htmlFor="username">username</label>
-                <input type="text" value="melvinoh" />
+                <input type="text"defaultValue="melvinoh" />
               </div>
               <div className="inputs-upt">
                 <label htmlFor="id">portfolio url</label>
-                <input type="text" value="https://melvinoh.co.ke" />
+                <input type="text"defaultValue="https://melvinoh.co.ke" />
               </div>
             </div>
            <div className="bio">
               <div className="inputs-upt">
                 <label htmlFor="id">bio</label>
-                <ReactQuill theme="snow" value={value} onChange={setValue} />
+                <ReactQuill theme="snow"defaultValue={value} onChange={setValue} />
               </div>
            </div>
             <div className="social-upt">
@@ -116,26 +116,26 @@ const ProfileUpdate = ({click}) => {
               <div className="inputs-wrap">
                 <div className="inputs-upt">
                   <label htmlFor="username">facebook</label>
-                  <input type="text" value="melvinoh" />
+                  <input type="url" defaultValue="https://melvinoh.co.ke" />
                 </div>
                 <div className="inputs-upt">
                   <label htmlFor="id">twiter</label>
-                  <input type="text" value="https://melvinoh.co.ke" />
+                  <input type="url" defaultValue="https://melvinoh.co.ke" />
                 </div>
               </div>
               <div className="inputs-wrap">
                 <div className="inputs-upt">
                   <label htmlFor="username">linkedIn</label>
-                  <input type="text" value="melvinoh" />
+                  <input type="url" defaultValue="https://melvinoh.co.ke" />
                 </div>
                 <div className="inputs-upt">
                   <label htmlFor="id">Instagram</label>
-                  <input type="text" value="https://melvinoh.co.ke" />
+                  <input type="url" defaultValue="https://melvinoh.co.ke" />
                 </div>
               </div>
               <div className="inputs-upt">
                 <label htmlFor="id">github</label>
-                <input type="text" value="https://melvinoh.co.ke" />
+                <input type="text" defaultValue="https://melvinoh.co.ke" />
               </div>
             </div>
           </div>
@@ -145,7 +145,7 @@ const ProfileUpdate = ({click}) => {
             </h2>
             <div className="inputs-upt">
                 <label htmlFor="id">skills</label>
-                <input type="text" value="" />
+                <input type="text" defaultValue="" />
             </div>
           </div>
         </div>
@@ -154,4 +154,4 @@ const ProfileUpdate = ({click}) => {
   )
 }
 
-export default ProfileUpdate
+ export default ProfileUpdate
